e9da8d381fc150e3db7f0c82721687113c27ce81
ueveton <uevetonsp89@gmail.com>
2024-12-03 12:30:18 -0300

    Configurando a tela de registro

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand m-auto text-light" href="#">Sistema de cadastro</a>
+                <Link className="navbar-brand m-auto text-light" to="/">
+                    Sistema de cadastro
+                </Link>
                 <div className="dropdown">
                     <button className="btn btn-danger dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Ueveton Soares
                     </button>
                     <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <Link className="dropdown-item" to="/registration">
+                                <i className="bi bi-people-fill p-2"></i>
+                                Perfil
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="dropdown-item" to="/login">
+                                <i className="bi bi-person-fill-gear p-2"></i>
+                                Sair
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </div>
--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import './Registration.css'
+import { Link } from 'react-router-dom'
 
 export const Registration = () => {
     return (
-        <div>
-            <h1>Registrando</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 offset-md-3">
+                    <div class="signup-form">
+                        <form action="" class="mt-5 border p-4 bg-light shadow">
+                            <h4 class="mb-5 text-secondary">Crie uma conta para acessar o sistema </h4>
+                            <div class="row">
+                                <div class="mb-3 col-md-12">
+                                    <label>Nome:<span class="text-danger">*</span></label>
+                                    <input type="text" name="fname" class="form-control" placeholder="Digite o seu nome"/>
+                                </div>
+                                <div class="mb-3 col-md-12">
+                                    <label>E-mail<span class="text-danger">*</span></label>
+                                    <input type="email" name="email" class="form-control" placeholder="Digite o seu e-mail"/>
+                                </div>
+                                <div class="mb-3 col-md-12">
+                                    <label>Senha<span class="text-danger">*</span></label>
+                                    <input type="password" name="password" class="form-control" placeholder="Digite a senha"/>
+                                </div>
+                                <div class="mb-3 col-md-12">
+                                    <label>Confirme a sua senha<span class="text-danger">*</span></label>
+                                    <input type="password" name="confirmpassword" class="form-control" placeholder="Confirme a sua senha"/>
+                                </div>
+                                <div class="col-md-12">
+                                    <button class="btn btn-primary float-end">Signup Now</button>
+                                </div>
+                            </div>
+                        </form>
+                        <p class="text-center mt-3 text-secondary">If you have account, Please <Link to="/login">Login Now</Link></p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
